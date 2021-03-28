1adbf79cf81581ae12c222c19f5a6763688dbd7b
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-03-29 00:11:03 +0200

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-content">
+          <p>© Neirouz</p>
+          <a
+            href="https://github.com/NeirouzJbira/Moviac"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Repository
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
