16dfe172ac70e12fe70c537eb27c2f90b0446ce9
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-03-29 00:11:14 +0200

    init

---
+++ src/components/Pagination.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Pagination = ({ page, totalPages }) => {
+  const url = window.location.pathname;
+
+  return (
+    <div className="pagination">
+      {page === 1 || page === 0 ? null : (
+        <Link to={`${url}?page=${page * 1 - 1}`} className="button">
+          Page {page * 1 - 1}
+        </Link>
+      )}
+      {page === totalPages ? null : (
+        <Link to={`${url}?page=${page * 1 + 1}`} className="button plus">
+          Page {page * 1 + 1}
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Pagination;
